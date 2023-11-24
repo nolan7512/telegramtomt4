00126d3a8ab471e7b5ed61812c3fc0f8f944de7d
nolan7512 <75007954+nolan7512@users.noreply.github.com>
2023-11-24 08:43:26 +0700

    Update run.py
    28

--- run.py
+++ run.py
@@ -186,16 +186,16 @@ from datetime import datetime
 
 async def create_table(data, is_pending=True):
     try:
-        # parsed_data = data[2]
+        #parsed_data = data[2]
         # logger.info('Create Table:')
         # logger.info(parsed_data)
         # Kiểm tra xem data có phải là chuỗi không
         if isinstance(data, str):
             # Nếu là chuỗi, chuyển đổi thành đối tượng Python
-            json_data = json.loads(data)
+            json_data = json.loads(data[2])
         elif isinstance(data, dict):
             # Nếu là từ điển, sử dụng trực tiếp
-            json_data = data
+            json_data = data[2]
         else:
             # Nếu không phải là chuỗi hoặc từ điển, xử lý lỗi hoặc trả về
             raise ValueError("Invalid data format")
