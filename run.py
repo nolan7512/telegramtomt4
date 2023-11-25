cfaac6001d22c38935960ff3592ef3cb0e87e6c0
nolan7512 <75007954+nolan7512@users.noreply.github.com>
2023-11-25 16:26:29 +0700

    Update run.py
    66

--- run.py
+++ run.py
@@ -296,15 +296,160 @@ async def open_trades(update: Update, context: CallbackContext) -> None:
     except Exception as e:
         update.effective_message.reply_text(f"Error open trades: {e}")
 
+
+# Function to handle the /trailingstop command
+async def trailing_stop(update: Update, context: CallbackContext) -> None:
+    # Get the string of position IDs from the command arguments
+    args = context.args
+    if not args:
+        update.message.reply_text("Please provide a list of position IDs.")
+        return
+
+    # Combine the arguments into a single string, then split it into a list of position IDs
+    position_ids = "".join(args).split(',')
+
+    api = MetaApi(API_KEY)
+    account = await api.metatrader_account_api.get_account(ACCOUNT_ID)
+    initial_state = account.state
+    deployed_states = ['DEPLOYING', 'DEPLOYED']
+
+    if initial_state not in deployed_states:
+        #  wait until account is deployed and connected to broker
+        logger.info('Deploying account')
+        await account.deploy()
+
+    logger.info('Waiting for API server to connect to broker ...')
+    await account.wait_connected()
+
+    # connect to MetaApi API
+    connection = account.get_rpc_connection()
+    await connection.connect()
+    await connection.wait_synchronized()
+    # Process each position ID
+    for position_id in position_ids:
+        try:
+            # Convert position ID to an integer (assuming it's a valid integer)
+            intposition_id = int(position_id)
+
+            # Get position information
+            position = await connection.get_position(intposition_id)
+
+            # Modify the position with trailing stop parameters
+            await connection.modify_position(
+                intposition_id,
+                stopLoss=position.openPrice,  # Set stopLoss to the openPrice
+                takeProfit=position.takeProfit  # Keep takeProfit unchanged
+            )
+
+            update.message.reply_text(f"Trailing stop set for position ID {intposition_id}. Successfully")
+
+        except ValueError:
+            update.message.reply_text(f"Invalid position ID: {intposition_id}. Please provide valid integers.")
+
+# Function to handle the /trailingstop command
+async def close_position(update: Update, context: CallbackContext) -> None:
+    # Get the string of position IDs from the command arguments
+    args = context.args
+    if not args:
+        update.message.reply_text("Please provide a list of position IDs.")
+        return
+
+    # Combine the arguments into a single string, then split it into a list of position IDs
+    position_ids = "".join(args).split(',')
+
+    api = MetaApi(API_KEY)
+    account = await api.metatrader_account_api.get_account(ACCOUNT_ID)
+    initial_state = account.state
+    deployed_states = ['DEPLOYING', 'DEPLOYED']
+
+    if initial_state not in deployed_states:
+        #  wait until account is deployed and connected to broker
+        logger.info('Deploying account')
+        await account.deploy()
+
+    logger.info('Waiting for API server to connect to broker ...')
+    await account.wait_connected()
+
+    # connect to MetaApi API
+    connection = account.get_rpc_connection()
+    await connection.connect()
+    await connection.wait_synchronized()
+    # Process each position ID
+    for position_id in position_ids:
+        try:
+            # Convert position ID to an integer (assuming it's a valid integer)
+            intposition_id = int(position_id)
+
+            # close position 
+            await connection.close_position(intposition_id)         
+            update.message.reply_text(f"Closed Position ID {intposition_id}. Successfully")
+
+        except ValueError:
+            update.message.reply_text(f"Invalid Position ID: {intposition_id}. Please provide valid integers.")
+# Function to handle the /trailingstop command
+async def close_position_partially(update: Update, context: CallbackContext) -> None:
+   # Get the string of position IDs and sizes from the command arguments
+    args = context.args
+    if not args or '|' not in args[0]:
+        update.message.reply_text("Please provide a list of position IDs and sizes separated by '|'.")
+        return
+
+    # Split the arguments into position IDs and sizes
+    position_args = args[0].split('|')
+    listID = position_args[0].split(',')
+    listSize = list(map(float, position_args[1].split(',')))
+    api = MetaApi(API_KEY)
+    account = await api.metatrader_account_api.get_account(ACCOUNT_ID)
+    initial_state = account.state
+    deployed_states = ['DEPLOYING', 'DEPLOYED']
+
+    if initial_state not in deployed_states:
+        #  wait until account is deployed and connected to broker
+        logger.info('Deploying account')
+        await account.deploy()
+
+    logger.info('Waiting for API server to connect to broker ...')
+    await account.wait_connected()
+
+    # connect to MetaApi API
+    connection = account.get_rpc_connection()
+    await connection.connect()
+    await connection.wait_synchronized()    
+    # Process each position ID and size
+    for i, position_id in enumerate(listID):
+        try:
+            # Convert position ID to an integer (assuming it's a valid integer)
+            intposition_id = int(position_id)
+
+            # Kiểm tra nếu không tồn tại phần tử tương ứng trong listSize
+            if i >= len(listSize):
+                update.message.reply_text(f"No size provided for Position ID {intposition_id}.")
+                break
+
+            size = float(listSize[i])
+
+            # Close a part of the position
+            await connection.close_position_partially(intposition_id, size)
+
+            update.message.reply_text(f"Closed a part of Position ID {intposition_id} successfully.")
+
+        except ValueError:
+            update.message.reply_text(f"Invalid position ID: {position_id}. Please provide valid integers.")
+        except Exception as e:
+            update.message.reply_text(f"Error closing part of Position ID {position_id}: {str(e)}.")
+
 def handle_pending_orders(update: Update, context: CallbackContext):
     asyncio.run(pending_orders(update,context))
 
 def handle_open_trades(update: Update, context: CallbackContext):
     asyncio.run(open_trades(update,context))
+
 def handle_trailingstop(update: Update, context: CallbackContext):
-    asyncio.run(open_trades(update,context))
+    asyncio.run(trailing_stop(update,context))
+
 def handle_closeposition(update: Update, context: CallbackContext):
-    asyncio.run(open_trades(update,context))
+    asyncio.run(close_position(update,context))
+
 # def find_entry_point(trade: str, signal: list[str], signaltype : str) -> float:
 #     first_line_with_order_type = next((i for i in range(len(signal)) if signal[i].upper().find(order_type_to_find, 0) != -1), -1)
 
@@ -1062,7 +1207,8 @@ def main() -> None:
     dp.add_handler(MessageHandler(Filters.command & Filters.regex('pendingorders'), handle_pending_orders))
     dp.add_handler(MessageHandler(Filters.command & Filters.regex('opentrades'), handle_open_trades))
     dp.add_handler(MessageHandler(Filters.command & Filters.regex('trailingstop'),handle_trailingstop ))
-    dp.add_handler(MessageHandler(Filters.command & Filters.regex('closeposition'),handle_closeposition ))
+    dp.add_handler(MessageHandler(Filters.command & Filters.regex('closeposition'),close_position ))
+    dp.add_handler(MessageHandler(Filters.command & Filters.regex('closepart'),close_position ))
     dp.add_handler(MessageHandler(Filters.text, TotalMessHandle))
 
     # log all errors
