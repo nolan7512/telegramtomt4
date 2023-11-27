afbbfeb1307c897aeff10df27ba9fc53620874fc
nolan7512 <75007954+nolan7512@users.noreply.github.com>
2023-11-27 20:06:35 +0700

    Update run.py
    8888

--- run.py
+++ run.py
@@ -1125,18 +1125,18 @@ def help(update: Update, context: CallbackContext) -> None:
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
 
-    help_message = "This AI bot is used to automatically enter trades onto your MetaTrader account directly from Telegram. To begin, ensure that you are authorized to use this bot by adjusting your Python script or environment variables.\n\nThis bot supports all trade order types (Market Execution, Limit, and Stop)\n"
+    help_message = "This AI bot is used to automatically enter trades onto your MetaTrader account directly from Telegram. To begin, ensure that you are authorized to use this bot by adjusting your Python script or environment variables.\n\nThis bot supports all trade order types (Market Execution, Limit, and Stop) and many functions : \n    + Check Account Infomation\n    + Check Opening Positions\n    + Pending Orders\n    + Set Trailing Stop\n    + Close All/Close Partially Position\n\n"
     #commands = "List of commands:\n/start : displays welcome message\n/help : displays list of commands and example trades\n/trade : takes in user inputted trade for parsing and placement\n/calculate : calculates trade information for a user inputted trade"
     #trade_example = "Example Trades 💴:\n\n"
     # market_execution_example = "Market Execution:\nBUY GBPUSD\nEntry NOW\nSL 1.14336\nTP 1.28930\nTP 1.29845\n\n"
     # limit_example = "Limit Execution:\nBUY LIMIT GBPUSD\nEntry 1.14480\nSL 1.14336\nTP 1.28930\n\n"
     # note = "You are able to enter up to two take profits. If two are entered, both trades will use half of the position size, and one will use TP1 while the other uses TP2.\n\nNote: Use 'NOW' as the entry to enter a market execution trade."
-    commandtrade = "Bot commands:\n/accountinfo : Check infomation account\n /open_trades : Check all Opening Position\n/pending_orders : Check all Pending Orders\n/closeposition id,id,id \n/closepart id,id|size,size \n/trailingstop id,id,id"
+    commandtrade = "\n----Bot commands:\n/accountinfo : Check infomation account\n /open_trades : Check all Opening Position\n/pending_orders : Check all Pending Orders\ncloseposition id,id,id \nclosepart id,id|size,size \ntrailingstop id,id,id"
     # sends messages to user
-    update.effective_message.reply_text(help_message)
+    update.effective_message.reply_text(help_message + commandtrade)
     #update.effective_message.reply_text(commands)
     # update.effective_message.reply_text(trade_example + market_execution_example + limit_example + note + commandtrade)
-    update.effective_message.reply_text(commandtrade)
+    #update.effective_message.reply_text(commandtrade)
     return
 
 def cancel(update: Update, context: CallbackContext) -> int:
